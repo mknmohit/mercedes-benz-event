47fb608c0a0d979a4022cd9448a8f6f755c681a5
Mohit Kumar <mohitkumar.7@outlook.com>
2020-05-19 11:41:57 +0530

    chore: initial setup

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,13 +6,9 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
 export default function HomePage() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <h1>Homepage</h1>
   );
 }
