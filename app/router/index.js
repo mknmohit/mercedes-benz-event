7519383d3284c234cc6aa79a6aa0a8937c0bb0de
Mohit Kumar <mohitkumar.7@outlook.com>
2020-05-20 18:57:37 +0530

    refactor: routes

---
+++ app/router/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import HomePage from 'containers/HomePage';
+import NotFoundPage from 'containers/NotFoundPage/Loadable';
+
+/* eslint-disable react/jsx-no-bind */
+function Router({ userData, isAuthenticated }) {
+  /* routeProps are passed by react-router-dom, routeProps includes {match: {…}, location: {…}, history: {…}, staticContext: undefined} */
+  return (
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={routeProps => <HomePage {...routeProps} userData={userData} isAuthenticated={isAuthenticated} />}
+      />
+      <Route component={NotFoundPage} />
+    </Switch>
+  );
+}
+
+Router.propTypes = {
+  userData: PropTypes.object,
+  isAuthenticated: PropTypes.bool,
+};
+
+export default Router;
