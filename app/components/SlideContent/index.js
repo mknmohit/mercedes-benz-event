d6820df3f1f17e7104b5646c3c88aea5b2193987
Mohit Kumar <mohitkumar.7@outlook.com>
2020-05-24 06:30:35 +0530

    chore: add functionality to play engine sound when holding peddle

--- app/components/SlideContent/index.js
+++ app/components/SlideContent/index.js
@@ -9,9 +9,23 @@ import PropTypes from 'prop-types';
 import { Animated } from "react-animated-css";
 
 import Slide1Img from 'images/slide-1-car.png';
+import Engine43cSound from 'audios/43-C-Engine.mp3';
 import Styled from './style';
 
 function SlideContent({ animate, index }) {
+
+  const engineSound = new Audio(Engine43cSound)
+
+  const playAudio = () => {
+    engineSound.play();
+    engineSound.loop = true;
+  }
+
+  const stopAudio = () => {
+    engineSound.pause()
+    engineSound.currentTime = 0;
+  }
+
   return (
     <div>
       <Styled.SlideImg src={Slide1Img} alt="car" />
@@ -19,7 +33,7 @@ function SlideContent({ animate, index }) {
         <Styled.ModelName>Mercedes-benz C 300</Styled.ModelName>
         <Styled.PeddleContainer>
           <Styled.PeddleInfo>Press the peddle to accelerate</Styled.PeddleInfo>
-          <Styled.Peddle>
+          <Styled.Peddle onMouseDown={playAudio} onMouseUp={stopAudio}>
             <Styled.PeddleImg />
           </Styled.Peddle>
         </Styled.PeddleContainer>
